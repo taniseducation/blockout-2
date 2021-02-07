3d7ae9c5512609b8f43c1c5ab10714c2b09c1a92
taniseducation <info@tanis.education>
2021-02-07 14:16:59 +0000

    zero

---
+++ index2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>hello
+        <?PHP
+            echo ' world!</h1> #';
+            // https://github.com/jlivingstonsg/blockout-2
+        ?>
+    </body>    
+</html>
